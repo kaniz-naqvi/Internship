d78dd19e69f5fb3f485afe57d462d1d629034a10
Kinz <161678979+kaniz-naqvi@users.noreply.github.com>
2024-10-17 15:43:00 +0500

    Add files via upload

---
+++ CodeAlpha_Project-IMAGE-GALLERY/gallery.js
@@ -0,0 +1,104 @@
+let main = document.querySelector("main");
+let sections = [
+  // Define the sections with their images
+  {
+    heading: "Flowers 🌸",
+    mainImg: "image1",
+    class: "mainFlowerImg",
+    firstImg: "image2",
+    secondImg: "image3",
+    function: "changeFlowerImg",
+    thirdImg: "image4",
+    forthImg: "image5"
+  },
+  {
+    heading: "Forest 🌳",
+    mainImg: "forest1",
+    class: "mainForestImg",
+    firstImg: "forest2",
+    secondImg: "forest3",
+    function: "changeForestImg",
+    thirdImg: "forest4",
+    forthImg: "forest5"
+  },
+  {
+    heading: "Sky ☁️🌤️",
+    mainImg: "sky1",
+    class: "mainSkyImg",
+    firstImg: "sky2",
+    secondImg: "sky3",
+    function: "changeSkyImg",
+    thirdImg: "sky4",
+    forthImg: "sky5"
+  }
+];
+
+// Create a section for each type of image
+sections.forEach((section, index) => {
+  main.innerHTML += `
+    <section>
+      <h1>${section.heading}</h1>
+      <div class="main-img">
+        <img src="gallery-images/${section.mainImg}.png" class="${section.class}" alt="${section.heading}">
+      </div>
+      <div class="other-imgs">
+        <button class="btn btn-primary left-arrow" onclick="changeImage(${index}, -1)"><i class="ri-arrow-left-wide-line"></i></button>
+        <img src="gallery-images/${section.mainImg}.png" alt="${section.heading}" onclick="${section.function}('gallery-images/${section.mainImg}.png')">
+        <img src="gallery-images/${section.firstImg}.png" alt="${section.heading}" onclick="${section.function}('gallery-images/${section.firstImg}.png')">
+        <img src="gallery-images/${section.secondImg}.png" alt="${section.heading}" onclick="${section.function}('gallery-images/${section.secondImg}.png')">
+        <img src="gallery-images/${section.thirdImg}.png" alt="${section.heading}" onclick="${section.function}('gallery-images/${section.thirdImg}.png')">
+        <img src="gallery-images/${section.forthImg}.png" alt="${section.heading}" onclick="${section.function}('gallery-images/${section.forthImg}.png')">     
+        <button class="btn btn-primary right-arrow" onclick="changeImage(${index}, 1)"><i class="ri-arrow-right-wide-line"></i></button>
+      </div>
+    </section>`;
+});
+
+// Initialize current indexes for each section
+let currentIndexes = sections.map(() => 0); // start with the first image (index 0)
+
+function changeImage(sectionIndex, direction) {
+  // Get the current section
+  let section = sections[sectionIndex];
+
+  // Calculate new index
+  currentIndexes[sectionIndex] = (currentIndexes[sectionIndex] + direction + 5) % 5; // 5 is the number of images
+
+  // Get the new image source based on the index
+  let newImgSrc;
+  if (currentIndexes[sectionIndex] === 0) {
+    newImgSrc = `gallery-images/${section.mainImg}.png`;
+  } else if (currentIndexes[sectionIndex] === 1) {
+    newImgSrc = `gallery-images/${section.firstImg}.png`;
+  } else if (currentIndexes[sectionIndex] === 2) {
+    newImgSrc = `gallery-images/${section.secondImg}.png`;
+  } else if (currentIndexes[sectionIndex] === 3) {
+    newImgSrc = `gallery-images/${section.thirdImg}.png`;
+  } else if (currentIndexes[sectionIndex] === 4) {
+    newImgSrc = `gallery-images/${section.forthImg}.png`;
+  }
+
+  // Change the main image based on the function specified
+  if (section.function === "changeFlowerImg") {
+    changeFlowerImg(newImgSrc);
+  } else if (section.function === "changeForestImg") {
+    changeForestImg(newImgSrc);
+  } else if (section.function === "changeSkyImg") {
+    changeSkyImg(newImgSrc);
+  }
+}
+
+// Functions to change the main image for each section
+let currentFlowerImg = document.querySelector(".mainFlowerImg");
+function changeFlowerImg(imgsrc) {
+  currentFlowerImg.setAttribute('src', imgsrc);
+}
+
+let currentForestImg = document.querySelector(".mainForestImg");
+function changeForestImg(imgsrc) {
+  currentForestImg.setAttribute('src', imgsrc);
+}
+
+let currentSkyImg = document.querySelector(".mainSkyImg");
+function changeSkyImg(imgsrc) {
+  currentSkyImg.setAttribute('src', imgsrc);
+}
---
+++ script.js
@@ -0,0 +1,25 @@
+let main=document.querySelector("section");
+let projects=[
+    {
+        projectLink:"CodeAlpha_Project-IMAGE-GALLERY/gallery.html", projrctName:"Image Gallery", livePreview:"", code:"",
+         projectDetails:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis cum iusto repudiandae consectetur recusandae distinctio nisi reiciendis necessitatibus laudantium explicabo dignissimos ab, tempore quam ducimus fugiat ea itaque facere perferendis.Ratione quia expedita recusandae quis, ab reprehenderit ex animi porro mollitia similique dolorem nostrum? Nobis sed ab ipsum corrupti. Quasi debitis vel explicabo numquam impedit cupiditate rem, mollitia veniam perferendis?Nam aliquid eius ad fuga sunt doloribus vitae. Fugit repellat tempore quidem iste voluptates, cupiditate saepe repudiandae fuga quo eum optio labore vero excepturi harum magni at laboriosam, illo pariatur.Eaque reiciendis porro nihil laudantium aspernatur placeat ab beatae in, voluptas, tenetur vitae rerum fugiat, suscipit consequuntur quas sequi omnis cum. Consequuntur, debitis corporis nulla dolore ad suscipit vitae distinctio!Laudantium provident repellendus a veritatis tempora corrupti maiores quod error nisi asperiores hic accusamus dignissimos, assumenda sed saepe est distinctio. Iure tempore quo incidunt velit! Quam id beatae reprehenderit corrupti!", 
+    }
+]
+projects.forEach(project => {
+    main.innerHTML=`<div>
+            <ol>
+                <h2><li><a href="${project.projectLink}">${project.projrctName}</a></li></h2>
+                <ul>
+                <li>
+                    <details>
+                        <summary>project details</summary>
+                        ${project.projectDetails}
+                    </details>
+                </li>
+                <li><a href="${project.livePreview}">Live preview</a></li>
+                <li><a href="${project.code}">Go to Code</a></li>
+                </ul>
+                
+            </ol>
+        </div>`;
+});
